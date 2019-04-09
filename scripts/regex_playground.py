aa9791047a34e0b4fc1afa2163404c43d83b5dbd
jcworkma <jack.workman@intel.com>
2019-04-08 22:42:58 -0700

    added walmart

--- scripts/regex_playground.py
+++ scripts/regex_playground.py
@@ -1,18 +1,22 @@
 import re
 import dateparser
+from wayback_search import get_update_date, REGEX_POLICY_DATE_LIST
 
 
 #with open('20110902041958_check_date.txt', 'r') as f:
-with open('20150420205418_check_date.txt', 'r') as f:
+with open('20091001221035_check_date.txt', 'r') as f:
     page = f.read()
 
 # This policy was last updated on July 21, 2011 ("Effective Date").
 # Last updated on July 21, 2011
 
-regex = re.compile(r'Revised: (\w+ \d+, \d+)')
-
-match = regex.search(page)
-print(match)
-print(match.group(1))
-update_date = dateparser.parse(match.group(1))
+update_date = get_update_date(page, regex_list=REGEX_POLICY_DATE_LIST)
 print(update_date)
+
+#regex = re.compile(r'Effective: (\w+ \d+, \d+)', flags=re.IGNORECASE)
+
+# match = regex.search(page)
+# print(match)
+# print(match.group(1))
+# update_date = dateparser.parse(match.group(1))
+# print(update_date)
